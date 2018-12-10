5047ce5fdfb13241940e579001ac5b9f323e1eda
Gurinov <evgeniy-gurinov@mail.ru>
2018-12-10 05:59:57 +0300

    Rewrite hard code in ConsoleApplication. Add test project.

--- ConsoleApplication/Program.cs
+++ ConsoleApplication/Program.cs
@@ -9,16 +9,18 @@ namespace ConsoleApplication
     {
         public static void Main(string[] args)
         {
-            List<string> filePaths = new List<string>()
+            List<string> filePaths = new List<string>
             {
-                @"C:\Users\Gurinov\Desktop\TestsGenerator\TestsGenerator\Action\FileReader.cs",
-                @"C:\Users\Gurinov\Desktop\TestsGenerator\TestsGenerator\Action\FileWriter.cs",
-                @"C:\Users\Gurinov\Desktop\TestsGenerator\TestsGenerator\model\Class.cs",
-                @"C:\Users\Gurinov\Desktop\TestsGenerator\TestsGenerator\model\Params.cs"
+                @"..\..\..\TestsGenerator\Action\FileWriter.cs",
+                @"..\..\..\TestsGenerator\Action\FileReader.cs",
+                @"..\..\..\TestsGenerator\Generator.cs",
+                @"..\..\..\TestsGenerator\SyntaxProcessor.cs",
+                @"..\..\..\TestsGenerator\TestsGenerator.cs"
             };
+            Params myParams = new Params(@"..\..\..\Tests", 2, 2, 2);
             TestsGenerator.TestsGenerator generator =
-                new TestsGenerator.TestsGenerator(filePaths, new Params(@"C:\Users\Gurinov\Desktop\q", 2, 2, 2));
-            FileWriter asyncWriter = new FileWriter(@"C:\Users\Gurinov\Desktop\q");
+                new TestsGenerator.TestsGenerator(filePaths, myParams);
+            FileWriter asyncWriter = new FileWriter(myParams.OutputDirectoryPath);
             generator.Generate(asyncWriter).Wait();
             Console.WriteLine("Success");
         }
